bae3763e8e2e249ae8e5c4287bce850bb91b866a
Nick Harrelson <nick.harrelson@gmail.com>
2017-10-30 19:58:32 -0400

    pivot

--- RScripts/CombineDatasets.R
+++ RScripts/CombineDatasets.R
@@ -8,6 +8,7 @@
 library(dplyr)
 library(caret)
 library(e1071)
+library(lubridate)
 
 # Read 911 call data
 datapath <- "./Data/PDI_Police_Calls_For_Service__CAD_.csv"
@@ -16,30 +17,16 @@ cinci <- read.csv(datapath, header = TRUE)
 forcepath <- "./Data/PDI_Use_of_Force.csv"
 UOF <- read.csv(forcepath, header = TRUE)
 
-# Get rid of empty columns
-cinci <- cinci %>% mutate(INCIDENT_TYPE_DESC = NULL) %>% mutate(PRIORITY_COLOR = NULL)
+# Get rid of empty columns, split date time into two columns
+cinci <- cinci %>% mutate(INCIDENT_TYPE_DESC = NULL) %>% 
+  mutate(PRIORITY_COLOR = NULL) %>% 
+  mutate(AGENCY = NULL) %>%
+  mutate(DATE = substr(CREATE_TIME_INCIDENT,1,10)) %>% 
+  mutate(TIME = substr(CREATE_TIME_INCIDENT,12,22)) %>% 
+  mutate(CREATE_TIME_INCIDENT = NULL)
 
 # Match up use of force incidents with call incidents
 matches <- cinci$EVENT_NUMBER %in% intersect(cinci$EVENT_NUMBER, UOF$CFS_NO)
 
 # Append this list to entire dataframe
 cinci <- cbind(cinci, as.data.frame(matches))
-
-set.seed(20160912)
-
-# Construct Partition
-# 70% reserved for training, 30% reserved for testing.
-# Balance across 5 quantile groups.
-inTrain <- createDataPartition(y = cinci$matches,
-                               p = 0.70,
-                               list = FALSE,
-                               groups = 5)
-
-Cinci.Train.df <- cinci %>% slice(inTrain)
-Cinci.Test.df <- cinci %>% slice(-inTrain)
-
-fit.CIrf <- train(matches ~ .,
-                  data=Cinci.Train.df,
-                  method="cforest",
-                  trControl=CIrf.cv,
-                  tuneGrid=CIrf.tuning.grid)
