67075a8ef71110430c363c3bb3a7457848464d09
Nick Harrelson <nick.harrelson@gmail.com>
2017-11-08 10:39:12 -0500

    Added point transformation in main code

--- RScripts/Model.R
+++ RScripts/Model.R
@@ -40,11 +40,28 @@ time.taken
 beep(8)
 
 # Plugging original model into the fresh 50x50 grid
-fifty_grid <- read.csv("./50x50Grid.csv", header = TRUE)
-new_predictions <- predict(trees, newdata = fifty_grid)
-fifty_grid <- fifty_grid %>% mutate(num_thefts = new_predictions)
+fifty_grid_orig <- read.csv("./50x50Grid.csv", header = TRUE)
+new_predictions <- predict(trees, newdata = fifty_grid_orig)
+fifty_grid_orig <- fifty_grid_orig %>% mutate(num_thefts = new_predictions)
 # write.csv(fifty_grid, "~/github/PoliceDataChallenge2017/Data/Predicted Grid.csv")
 
+transform_points <- function(df){
+  df <- df %>% mutate(num_thefts = round(num_thefts))
+  for(i in 1:nrow(df)){
+    temp_x <- rep(df$x_repr[i],df$num_thefts[i])
+    temp_y <- rep(df$y_repr[i],df$num_thefts[i])
+    temp_day <- rep(df$day[i],df$num_thefts[i])
+    temp_hour <- rep(df$hour[i],df$num_thefts[i])
+    temp_df <- data.frame(x=temp_x, y=temp_y, day = temp_day, hour = temp_hour)
+    if(i == 1){
+      new_data <- temp_df
+    } else{
+      new_data <- rbind(new_data,temp_df)
+    }
+  }
+}
+
+fifty_grid <- transform_points(fifty_grid_orig)
 
 # Constructing static images
 seattle_google_map <- get_map("seattle", zoom = 11, maptype = "roadmap", color = "bw")
--- RScripts/transform_points.R
+++ RScripts/transform_points.R
@@ -1,9 +1,11 @@
 transform_points <- function(df){
   df <- df %>% mutate(num_thefts = round(num_thefts))
-  for(i in 1:nrows(df)){
+  for(i in 1:nrow(df)){
     temp_x <- rep(df$x_repr[i],df$num_thefts[i])
     temp_y <- rep(df$y_repr[i],df$num_thefts[i])
-    temp_df <- data.frame(x=temp_x, y=temp_y)
+    temp_day <- rep(df$day[i],df$num_thefts[i])
+    temp_hour <- rep(df$hour[i],df$num_thefts[i])
+    temp_df <- data.frame(x=temp_x, y=temp_y, day = temp_day, hour = temp_hour)
     if(i == 1){
       new_data <- temp_df
     } else{
